4badd6b91b17ec0430488dd788e23045ffb593fa
Kağan <cckagan@gmail.com>
2026-02-01 01:23:33 +0300

    Add pitch option to join-sound configuration

--- src/main/java/dev/naw0ke/wrusmp/listener/VanishListener.java
+++ src/main/java/dev/naw0ke/wrusmp/listener/VanishListener.java
@@ -6,7 +6,6 @@ import dev.naw0ke.wrusmp.WruJoinPlugin;
 import dev.naw0ke.wrusmp.util.ChatUtils;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,23 +18,21 @@ public class VanishListener implements Listener {
     @EventHandler
     public void onVanish(PlayerHideEvent event) {
         Player player = event.getPlayer();
-        for (Player online : Bukkit.getOnlinePlayers()) {
-            online.sendActionBar(
-                    ChatUtils.format(plugin.getConfig().getString("settings.quit-message"),
-                            Placeholder.unparsed("player", player.getName())
-                    ));
-        }
+
+        plugin.getServer().sendActionBar(
+                ChatUtils.format(plugin.getConfig().getString("settings.quit-message"),
+                        Placeholder.unparsed("player", player.getName())
+                ));
     }
 
     @EventHandler
     public void onUnVanish(PlayerShowEvent event) {
         Player player = event.getPlayer();
-        for (Player online : Bukkit.getOnlinePlayers()) {
-            online.sendActionBar(
-                    ChatUtils.format(plugin.getConfig().getString("settings.join-message"),
-                            Placeholder.parsed("player", player.getName())
-                    ));
-        }
+
+        plugin.getServer().sendActionBar(
+                ChatUtils.format(plugin.getConfig().getString("settings.join-message"),
+                        Placeholder.unparsed("player", player.getName())
+                ));
     }
 
 }
